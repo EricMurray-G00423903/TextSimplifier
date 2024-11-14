48573c3570f287365c4ac483db8d77cd830bd7a1
EricMurray-G00423903 <g00423903@atu.ie>
2024-11-14 01:19:51 +0000

    worked on TextProcessor - filled out simplfiyText method and logic - changed class structure removed redundant code

--- TextSimplifier/src/ie/atu/sw/TextProcessor.java
+++ TextSimplifier/src/ie/atu/sw/TextProcessor.java
@@ -5,57 +5,74 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class TextProcessor {
 	
-	Map<String, double[]> embeddings = new ConcurrentHashMap<>();
-	Set<String> googleWords = new HashSet<>();
+	Map<String, double[]> googleWordsMap = new ConcurrentHashMap<>();	//CCHashMaps for both Embeddings Map and Google Map
+	Map<String, double[]> embeddingsMap = new ConcurrentHashMap<>();
 	
-	public TextProcessor(Map<String, double[]> embeddings, Set<String> googleWords) {
+	public TextProcessor(Map<String, double[]> googleWordsMap, Map<String, double[]> embeddingsMap) {	//Constructor to assign maps
 		
-		this.embeddings = embeddings;
-		this.googleWords = googleWords;
+		this.googleWordsMap = googleWordsMap;
+		this.embeddingsMap = embeddingsMap;
 		
 	}
 	
-	public static void simplifyText(String inputPath, String outputPath, Map<String, double[]> embMap, Set<String> googleSet) throws IOException {
+	public void simplifyText(String inputPath, String outputPath) throws IOException {
 		
-		BufferedReader reader = new BufferedReader(new FileReader(inputPath));
-		BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath));
+		try (BufferedReader reader = new BufferedReader(new FileReader(inputPath));		//Wrapped Buffered reader to close even if exception thrown
+			 BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath))) {
 		
-		String line;
-		
-		while((line = reader.readLine()) != null) {
+			String line;
 			
-			String[] splitLines = line.split("((?=\\p{Punct})|(?<=\\p{Punct})|\\s+)");
-			
-			StringBuilder sb = new StringBuilder();
-			
-			for (int i = 0; i < splitLines.length; i++) {
+			while((line = reader.readLine()) != null) {
 				
-				if (splitLines[i].matches("\\p{Punct}+")) {
-					sb.append(splitLines[i]);
+				String[] splitLines = line.split("((?=\\p{Punct})|(?<=\\p{Punct})|\\s+)");	//Regex to split lines by punctuation and by white spaces
+				
+				StringBuilder sb = new StringBuilder();	//Create a String Builder
+				
+				for (int i = 0; i < splitLines.length; i++) {	//Iterate over the split Line
+					
+					if (splitLines[i].matches("\\p{Punct}+")) {
+						sb.append(splitLines[i] + " ");		//If the current string[] index is a punctation char then just append to the stringbuilder
+					}
+					
+					if (this.googleWordsMap.containsKey(splitLines[i])) {	//If the word already in the google Map then just append to the sb
+						
+						sb.append(splitLines[i] + " ");	//append with a whitespace after the word
+						
+					} else {	//if its not in the google Map
+						
+						if (this.embeddingsMap.containsKey(splitLines[i])) {	//and it is in the embeddings map
+							
+							String newWord = findClosestWord(splitLines[i]);	//find the closest word within the googleMap
+							sb.append(newWord + " ");	//append the new word from the google map to the sb
+							
+						} else {
+							
+							sb.append(splitLines[i] + " ");	//else if the word doesnt exist in either map just append
+							
+						}
+						
+					}
+					
 				}
 				
-				if (googleSet.contains(splitLines[i])) {
-					
-					sb.append(splitLines[i] + " ");
-					
-				} else {
-					//implement find closest match from embeddings
-				}
+				writer.write(sb.toString());	//write the current sb String to the line
+				writer.newLine();				//Move to the next line
 				
 			}
-			
 		}
 		
+	}
+	
+	public String findClosestWord(String wordToChange) {
 		
-		reader.close();
-		writer.close();
+		String closestWord = null;
+		
+		return closestWord;
 		
 	}
 
